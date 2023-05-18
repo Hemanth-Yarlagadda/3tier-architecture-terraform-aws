ddaf5452f1425f310ea6ab04a6171ce91c506a11
Hemanth_Yarlagadda <yarlaghh@mail.uc.edu>
2023-05-18 01:43:37 -0500

    changed bg color

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "template_cloudinit_config" "config" {
     ALB_DNS=${module.alb.lb_dns_name}
     
     sudo echo '<h1>Sample App Created by ASG with ALB - APP-1</h1>' | sudo tee /var/www/html/index.html
-    sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Sample App created by ASG with ALB  - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+    sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(219, 205, 205);"> <h1>Sample App created by ASG with ALB  - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
 
     systemctl restart nginx
     systemctl status nginx
